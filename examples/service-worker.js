70d8ce87480833a0bb805d0aabf5d22249a7664b
matiboy <mathieu.chauvinc@gmail.com>
2015-08-25 12:16:34 +0800

    Add service worker example

---
+++ examples/service-worker.js
@@ -0,0 +1,40 @@
+'use strict';
+
+self.addEventListener('push', function(event) {
+  console.log('Received a push message', event);
+  var title = 'Anacondapp';
+  var body = JSON.stringify(event);
+  var icon = '/images/icon-192x192.png';
+  var tag = 'simple-push-demo-notification-tag';
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      icon: icon,
+      tag: tag
+    })
+  );
+});
+
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ', event);
+  // Android doesn’t close the notification when you click on it
+  // See: http://crbug.com/463146
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url == '/' && 'focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow)
+      return clients.openWindow('/');
+  }));
+
+});
